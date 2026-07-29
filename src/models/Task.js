ff5ce94676d3ca636b7c7be59c91a1e889d19b08
Esteban Vargas Quiros <79805608+estebanvquiros@users.noreply.github.com>
2026-07-29 12:28:56 -0600

    Add Task model

---
+++ src/models/Task.js
@@ -0,0 +1,10 @@
+export default class Task {
+  constructor(id, title, description, dueDate, priority, projectID) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.projectID = projectID;
+  }
+}
