3905f090fc38e2b344eab919e46b0b97c5072b52
Esteban Vargas Quiros <79805608+estebanvquiros@users.noreply.github.com>
2026-09-05 15:21:11 -0600

    Remove task description and dueDate when empty
    This avoids creating empty html elements
    when the description and dueDate are not
    specified.

--- src/views/taskView.js
+++ src/views/taskView.js
@@ -2,29 +2,35 @@ function createTaskElement(task) {
   const taskItem = document.createElement("li");
   const checkbox = document.createElement("input");
   const taskTitle = document.createElement("h2");
-  const taskDescription = document.createElement("p");
-  const taskDueDate = document.createElement("p");
   const taskPriority = document.createElement("p");
 
   taskItem.classList.add("task");
   checkbox.classList.add("task-checkbox");
   checkbox.type = "checkbox";
   taskTitle.classList.add("task-title");
-  taskDescription.classList.add("task-description");
-  taskDueDate.classList.add("task-duedate")
   taskPriority.classList.add("task-priority");
 
   taskItem.dataset.taskId = task.id;
   taskTitle.textContent = task.title;
-  taskDescription.textContent = task.description;
-  taskDueDate.textContent = task.dueDate;
   taskPriority.textContent = task.priority;
 
   taskItem.appendChild(checkbox);
   taskItem.appendChild(taskTitle);
-  taskItem.appendChild(taskDescription);
-  taskItem.appendChild(taskDueDate);
   taskItem.appendChild(taskPriority);
+
+  if (task.dueDate) {
+    const taskDueDate = document.createElement("p");
+    taskDueDate.classList.add("task-duedate")
+    taskDueDate.textContent = task.dueDate;
+    taskItem.appendChild(taskDueDate);
+  }
+
+  if (task.description) {
+    const taskDescription = document.createElement("p");
+    taskDescription.classList.add("task-description");
+    taskDescription.textContent = task.description;
+    taskItem.appendChild(taskDescription);
+  }
 
   return taskItem;
 }
